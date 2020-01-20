d8ca56a066c372616c448a97fab0ea11ca4faab9
Shashank Shekhar <sshekhar336@gmail.com>
2020-01-20 22:48:35 +0530

    Updates

---
+++ precache-manifest.4c1a3f81c6fdbfbd69106666383e6046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45fbcaf810ee2cafb9a1c7f61d2ee57",
+    "url": "/chatnet-app/index.html"
+  },
+  {
+    "revision": "478dfad183a87e5bcba4",
+    "url": "/chatnet-app/static/css/main.252f3d50.chunk.css"
+  },
+  {
+    "revision": "8663ee73c205fad1407f",
+    "url": "/chatnet-app/static/js/2.f5523383.chunk.js"
+  },
+  {
+    "revision": "e5adfe65f2baa231f34792ba8f0584e3",
+    "url": "/chatnet-app/static/js/2.f5523383.chunk.js.LICENSE"
+  },
+  {
+    "revision": "478dfad183a87e5bcba4",
+    "url": "/chatnet-app/static/js/main.93bf05d7.chunk.js"
+  },
+  {
+    "revision": "c7f3a4a2dc4931c619aa",
+    "url": "/chatnet-app/static/js/runtime-main.509b8ba6.js"
+  }
+]);
